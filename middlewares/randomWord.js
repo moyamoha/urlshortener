c866b03c993209f82f25612483d059856b01fa04
moyamoha <82891624+moyamoha@users.noreply.github.com>
2022-03-11 11:34:30 +0200

    generate actual words instead of random strings

--- middlewares/randomWord.js
+++ middlewares/randomWord.js
@@ -1,12 +1,11 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
 async function getRandomWord(req, res, next) {
 	try {
-		const response = await fetch(
+		const response = await axios.get(
 			"https://random-word-api.herokuapp.com/word?number=1&swear=0 "
 		);
-		const data = response.json();
-		res.random = data[0];
+		res.word = response.data[0];
 		next();
 	} catch (err) {
 		console.log(err);
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import urlRouter from "./routes/url.js";
+import Url from "./models/url.js";
+import getRandomWord from "./middlewares/randomWord.js";
 
 dotenv.config();
 
@@ -24,7 +25,35 @@ mongoose
 	)
 	.catch((error) => console.log(error));
 
-app.use("/url", urlRouter);
+app.post("/", getRandomWord, async (req, res) => {
+	if (!req.body || !req.body.url) {
+		res.status(400).json({ error: "Should provide a url" });
+		return;
+	}
+	const randomWord = res.word;
+	const newUrlRecord = new Url({
+		original: req.body.url,
+		identifier: randomWord,
+	});
+	try {
+		const savedUrl = await newUrlRecord.save();
+		res.status(201).json({
+			shortened: `https://lyhenna.herokuapp.com/${savedUrl.identifier}/`,
+		});
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+app.get("/[^]{2,}", async (req, res) => {
+	console.log(req.url.split("/")[1]);
+	try {
+		const foundRoute = await Url.findOne({ identifier: req.url.split("/")[2] });
+		res.redirect(foundRoute.original);
+	} catch (err) {
+		console.log(err.message);
+	}
+});
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
